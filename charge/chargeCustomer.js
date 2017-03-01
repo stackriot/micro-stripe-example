cd4e53995e5c494bed5213c3ae4baf5b67b9da70
Martin Adams <heymartinadams@users.noreply.github.com>
2017-03-01 08:30:00 -0800

    Added secure key and logging for charge (#3)
    * Added secure key and logging
    
    * Update chargeCustomer.js
    
    * Update chargeCustomer.js

--- charge/chargeCustomer.js
+++ charge/chargeCustomer.js
@@ -1,48 +1,61 @@
 const { json, send } = require('micro')
 const request = require('request')
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
+const log = process.env.LOG
+require('now-logs')(log);
 
 const endpoint = process.env.ENDPOINT
 const graphcoolToken = `Bearer ${process.env.GC_PAT}`
+const secureKey = process.env.SECUREKEY
 
 module.exports = async (req, res) => {
   const data = await json(req)
-  const purchase = data.createdNode
-  const purchaseId = purchase.id
-  const customerId = purchase.user.stripeId
+  
+  const { parse } = require('url');
+  const { query } = parse(req.url, true)
 
-  if (purchase.isPaid) {
-    send(res, 400, { error: `Customer ${customerId} could not be charged, because purchase ${purchaseId} was already paid` })
-  }
+  if (secureKey === query.securekey) {
+    const purchase = data.createdNode
+    const purchaseId = purchase.id
+    const customerId = purchase.user.stripeId
 
-  var charge = stripe.charges.create({
-    amount: purchase.amount,
-    currency: 'usd',
-    description: purchase.description,
-    customer: customerId,
-  }, (err, charge) => {
-    if (err) {
-      console.log(err)
-      send(res, 400, { error: `Customer ${customerId} could not be charged` })
-    } else {
-      const mutation = `mutation {
-        updatePurchase(id: "${purchaseId}", isPaid: true) {
-          id
-        }
-      }`
+  // Add logs during development, but remove for production
+  // console.log('Purchase Object');
+  // console.log(purchase);
 
-      request.post({
-        url: endpoint,
-        headers: {
-          'Authorization' : graphcoolToken,
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({query: mutation}),
-      }).on('error', (e) => {
-        send(res, 400, { error: `Customer ${customerId} was charged, but purchase ${purchaseId} was not marked as paid` })
-      }).on('response', (response) => {
-        send(res, 200, { message: `Customer ${customerId} was charged and purchase ${purchaseId} was marked as paid` })
-      })
+    if (purchase.isPaid) {
+      send(res, 400, { error: `Customer ${customerId} could not be charged, because purchase ${purchaseId} was already paid` })
     }
-  })
+
+    var charge = stripe.charges.create({
+      amount: purchase.amount,
+      currency: 'usd',
+      description: purchase.description,
+      customer: customerId,
+    }, (err, charge) => {
+      if (err) {
+        console.log(err)
+        send(res, 400, { error: `Customer ${customerId} could not be charged` })
+      } else {
+        const mutation = `mutation {
+          updatePurchase(id: "${purchaseId}", isPaid: true) {
+            id
+          }
+        }`
+
+        request.post({
+          url: endpoint,
+          headers: {
+            'Authorization' : graphcoolToken,
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({query: mutation}),
+        }).on('error', (e) => {
+          send(res, 400, { error: `Customer ${customerId} was charged, but purchase ${purchaseId} was not marked as paid` })
+        }).on('response', (response) => {
+          send(res, 200, { message: `Customer ${customerId} was charged and purchase ${purchaseId} was marked as paid` })
+        })
+      }
+    })
+  }
 }
