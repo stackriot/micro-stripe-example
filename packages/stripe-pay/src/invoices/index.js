d59cceccd0172b99d75d14b0b4dcf71f2cd2e8c7
Kristian Mandrup <kmandrup@gmail.com>
2017-06-08 21:46:30 +0200

    more schemas

--- packages/stripe-pay/src/invoices/index.js
+++ packages/stripe-pay/src/invoices/index.js
@@ -15,6 +15,39 @@ class Invoices extends Collection {
     this.handleError('Invoices can NOT be deleted', id)
   }
 
-  // amount_due
-  // application_fee
+  get schema() {
+    return {
+      type: 'object',
+      properties: {
+        amount_due: $money,
+        application_fee: $money,
+        attempt_count: num(0, 20),
+        attempted: bool,
+        charge: $id,
+        closed: bool,
+        currency,
+        customer: $id,
+        date,
+        description,
+        discount: obj(),
+        ending_balance: money,
+        forgiven: bool,
+        lines: list(),
+        paid: bool,
+        period_end: date,
+        period_start: date,
+        receipt_number: $id,
+        starting_balance: money,
+        statement_descriptor: description,
+        subscription: $is,
+        subscription_proration_date: date,
+        subtotal: $money,
+        tax: $money,
+        tax_percent: percent,
+        total: $money,
+        webhooks_delivered_at: date
+      },
+      required: ['amount_due', 'total']
+    }
+  }
 }
--- packages/stripe-pay/src/products/index.js
+++ packages/stripe-pay/src/products/index.js
@@ -10,4 +10,27 @@ export class Products extends Collection {
   constructor(config, opts) {
     super('Product', 'products', config, opts)
   }
+
+  get schema() {
+    return {
+      type: 'object',
+      properties: {
+        active: bool,
+        attributes: list(),
+        caption: name,
+        created: date,
+        description,
+        images: list(),
+        name,
+        package_dimensions: obj(),
+        shippable: bool,
+        skus: list(),
+        has_more: bool,
+        total_count: num(0, 100),
+        url,
+        updated: date
+      },
+      required: ['name']
+    }
+  }
 }
